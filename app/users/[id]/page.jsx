5e5a4a24badbfa4aa1c6efe9d24a476f980e0a76
Ray <buttrafay980@gmail.com>
2023-11-25 18:50:32 +0500

    updating profile

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -10,7 +10,7 @@ import Image from 'next/image';
 const Profile = ( { params } ) => {
 
   const { data: session } = useSession();
-  const [ user, setUser ] = useState( { name: "", email: "", image: "", id: 0 } );
+  const [ user, setUser ] = useState( { name: "", email: "", image: "", id: 0, created_at: "" } );
 
   useEffect( () => {
     if ( session?.user.id != params.id ) {
@@ -23,6 +23,7 @@ const Profile = ( { params } ) => {
           email: body.email,
           image: body.image,
           id: parseInt( body.id ),
+          created_at: body.created_at
         } );
       };
 
@@ -32,30 +33,46 @@ const Profile = ( { params } ) => {
 
 
   return (
-    <section id={ styles[ "profile" ] }>
-      <div className={ styles[ "user-info" ] }>
-        <div className={ styles[ "img-name" ] }>
-          <Image
-            alt='pfp'
-            src={ params.id == session?.user.id ? session?.user.image : user.image }
-            width={ 60 }
-            height={ 60 }
-          />
-          <p className={ styles[ "name" ] }>{ params.id == session?.user.id ? session?.user.name : user.name }</p>
+    <>
+      <section id={ styles[ "intro" ] }>
+        <p className={ styles[ "intro-name" ] }>{ user.name }</p>
+        <Image
+          src={ "/Images/bubbles.svg" }
+          width={ 300 }
+          height={ 150 }
+          alt='bubbles'
+          className={ styles[ 'bubbles' ] }
+        />
+      </section>
+      <section id={ styles[ "profile" ] }>
+        <div className={ styles[ "user-info" ] }>
+          <div className={ styles[ "img-name" ] }>
+            <Image
+              alt='pfp'
+              src={ params.id == session?.user.id ? session?.user.image : user.image }
+              width={ 60 }
+              height={ 60 }
+            />
+            <p className={ styles[ "name" ] }>{ params.id == session?.user.id ? session?.user.name : user.name }</p>
+          </div>
+          <hr className={ styles[ 'hr' ] } />
+          <div className={ styles[ "stats" ] }>
+          </div>
+          <div className={ styles[ "account-date" ] }>
+            <p>Account made on:</p>
+            <span>{ user.created_at }</span>
+          </div>
+          <hr className={ styles[ 'hr' ] } />
+          <button className={ styles[ 'delete-btn' ] }>Delete my account</button>
         </div>
-        <hr className={ styles[ 'hr' ] } />
-        <div className={ styles[ "stats" ] }>
+        <div className={ styles[ "posts" ] }>
+          <h1 className={ styles[ "title" ] }>
+            { params.id == session?.user.id ? "My Posts" : <>{ user.name + "'s Posts" }</> }
+          </h1>
+          <Feed user_ID={ params.id } className={ styles[ "feed" ] } />
         </div>
-        <hr className={ styles[ 'hr' ] } />
-        <button className={ styles[ 'delete-btn' ] }>Delete my account</button>
-      </div>
-      <div className={ styles[ "posts" ] }>
-        <h1 className={ styles[ "title" ] }>
-          { params.id == session?.user.id ? "My Posts" : <>{ user.name + "'s Posts" }</> }
-        </h1>
-        <Feed user_ID={ params.id } className={ styles[ "feed" ] } />
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
