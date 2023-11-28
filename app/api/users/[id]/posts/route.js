d95987616d68ca22efa5ab320d0f9d932abb718f
Ray <buttrafay980@gmail.com>
2023-11-28 15:56:12 +0500

    updating

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -1,23 +1,26 @@
 import { getData } from "@/app/Supabase/Supabase";
 
-export const GET = async (req, { params }) => {
+export const POST = async ( req, { params } ) => {
+
+  const { id } = params;
+
   try {
-    
-    const body = await getData({
+
+    const body = await getData( {
       table: "posts",
       where: {
-        userID: params.id
+        userID: id
       }
-    });
+    } );
 
-    if (!body?.data?.length) return new Response("Posts Not Found", { status: 404 })
-    
-    return new Response(JSON.stringify(body.data), { status: 200 });
+    if ( !body?.data?.length ) return new Response( "Posts Not Found", { status: 404 } );
 
-  } catch (err) {
+    return new Response( JSON.stringify( body.data ), { status: 200 } );
 
-    console.log(err);
-    return new Response("Failed to get Posts", { status: 500 });
+  } catch ( err ) {
+
+    console.log( err );
+    return new Response( "Failed to get Posts", { status: 500 } );
 
   }
-}
+};
