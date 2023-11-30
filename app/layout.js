9977764c31a20ec6d44e1d4b3bdd0cc9deb52a96
Ray <buttrafay980@gmail.com>
2023-11-30 15:02:55 +0500

    updain

--- app/layout.js
+++ app/layout.js
@@ -1,30 +1,34 @@
 import Provider from '@/Provider/Provider.jsx';
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
 import Navbar from './Components/Navbar/Navbar.jsx';
+
 
 import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter( { subsets: [ 'latin' ] } );
 
 export const metadata = {
   title: 'VioSell: Your Market',
   description: 'VioSell: Your Marketplace.',
+  openGraph: {
+    images: [ "https://viosell.vercel.app/images/favicon.svg" ]
+  }
   // icons: {
   //   icon: "favicon.ico"
   // }
-}
+};
 
-export default function RootLayout({ children }) {
+export default function RootLayout ( { children } ) {
   return (
     <html lang="en">
       <body className={ inter.className }>
         <Provider>
           <Navbar />
           <main>
-            {children}
+            { children }
           </main>
         </Provider>
       </body>
     </html>
-  )
+  );
 }
