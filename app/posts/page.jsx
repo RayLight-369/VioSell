9910bbb570a361060d77ea764da0c508865622f3
RayLight-369 <abdulrafay.designs@gmail.com>
2024-02-06 23:23:32 +0500

    input space coorect

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -6,40 +6,46 @@ import Feed from "../Components/Feed/Feed";
 import styles from "./posts.module.css";
 
 const page = () => {
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
 
-  const [ query, setQuery ] = useState( "" );
-  const [ data, setData ] = useState( [] );
-
-  const handleSearch = async ( e, Range, filter ) => {
-    if ( e.key == "Enter" && query.trim().length ) {
-
-      const request = await fetch( "/api/posts/search", {
+  const handleSearch = async (e, Range, filter) => {
+    if (e.key == "Enter" && query.trim().length) {
+      const request = await fetch("/api/posts/search", {
         method: "POST",
-        body: JSON.stringify( {
+        body: JSON.stringify({
           query,
           range: Range,
-          filter: filter ? filter : "relevance"
-        } ),
-      } );
+          filter: filter ? filter : "relevance",
+        }),
+      });
 
-      if ( request.ok ) {
+      if (request.ok) {
         let body = await request.json();
-        console.log( body );
-        setData( prev => body.data );
+        console.log(body);
+        setData((prev) => body.data);
       }
-
     }
   };
 
-  useEffect( () => {
-    if ( !query.trim().length ) {
-      setData( [] );
+  useEffect(() => {
+    if (!query.trim().length) {
+      setData([]);
     }
-  }, [ query ] );
+  }, [query]);
 
   return (
-    <section id={ styles[ "feed" ] }>
-      <Feed type={ "all" } data={ data.length ? data : null } range={ [ 0, 19 ] } newPostsWhileScrolling={ true } searchBar query={ query.trim() } setQuery={ setQuery } handleSearch={ handleSearch } />
+    <section id={styles["feed"]}>
+      <Feed
+        type={"all"}
+        data={data.length ? data : null}
+        range={[0, 19]}
+        newPostsWhileScrolling={true}
+        searchBar
+        query={query}
+        setQuery={setQuery}
+        handleSearch={handleSearch}
+      />
     </section>
   );
 };
